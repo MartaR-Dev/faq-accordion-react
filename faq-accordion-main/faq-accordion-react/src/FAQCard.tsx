a96eb931627b7ef1b950debe07710cb7d377f8fe
Marta <martadottiyt@hotmail.com>
2024-05-05 17:46:58 +0200

    Final styling
    Adjust padding and margings, font sizes, opacity, etc
    (before any possible additional feedback)

--- faq-accordion-main/faq-accordion-react/src/FAQCard.tsx
+++ faq-accordion-main/faq-accordion-react/src/FAQCard.tsx
@@ -5,21 +5,21 @@ import { questionsAndAnswers } from "./Questions_Answers";
 export const FAQCard = () => {
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="card-base p-5">
-        <div className="card-header d-flex flex-row mb-3">
-          <img src={starIcon} alt="Star icon" className="me-3" />
+      <div className="card-base px-4 pt-5 pb-3">
+        <div className="card-header d-flex flex-row mb-4 ms-3">
+          <img src={starIcon} alt="Star icon" className="me-4" />
           <h1 id="card-title" className="fw-bold">
             FAQs
           </h1>
         </div>
 
         {questionsAndAnswers.map(({ question, answer }, index) => (
-          <>
+          <div className="mx-3">
             <AccordionItemFAQ question={question} answer={answer} />
             {index !== questionsAndAnswers.length - 1 && (
               <hr className="my-1" />
             )}
-          </>
+          </div>
         ))}
       </div>
     </div>
